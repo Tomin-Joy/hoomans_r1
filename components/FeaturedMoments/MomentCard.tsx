f2eef824c1dd197485d1b6ad57406e5b158137e1
tomin-joy <tominjk007@gmail.com>
2024-11-14 21:42:00 +0530

    assignment

--- components/FeaturedMoments/MomentCard.tsx
+++ components/FeaturedMoments/MomentCard.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { cn } from '@/lib/utils';
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 interface MomentCardProps {
   title: string;
@@ -10,7 +11,13 @@ interface MomentCardProps {
   index: number;
 }
 
-export default function MomentCard({ title, description, image, isActive, index }: MomentCardProps) {
+export default function MomentCard({
+  title,
+  description,
+  image,
+  isActive,
+  index,
+}: MomentCardProps) {
   return (
     <div
       className={cn(
@@ -18,21 +25,24 @@ export default function MomentCard({ title, description, image, isActive, index 
         isActive && "lg:w-[420px] md:w-[340px] "
       )}
       style={{
-        animationDelay: `${index * 100}ms`
+        animationDelay: `${index * 100}ms`,
       }}
     >
       <div className="relative overflow-hidden rounded-xl aspect-[3/2]">
-        <img
+        <Image
+          fetchPriority="high"
+          fill
           src={image}
           alt={title}
           className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110"
         />
         <div className="absolute inset-0 bg-black/20 group-hover:bg-black/40 transition-colors duration-300" />
       </div>
-      <div className="mt-4 animate-fade-in" style={{ animationDelay: `${index * 200}ms` }}>
-        <h3 className="text-lg font-medium text-gray-900 mb-1">
-          {title}
-        </h3>
+      <div
+        className="mt-4 animate-fade-in"
+        style={{ animationDelay: `${index * 200}ms` }}
+      >
+        <h3 className="text-lg font-medium text-gray-900 mb-1">{title}</h3>
         <p className={`text-sm text-gray-600 ${isActive || "line-clamp-2"}`}>
           {description}
         </p>
